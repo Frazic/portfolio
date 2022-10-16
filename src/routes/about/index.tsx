3c199aab320a19d53e68a3bc2c0ea57ad89a340a
Frazic <bruylant.joshua@gmail.com>
2022-10-16 10:28:13 +0200

    merged

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -6,6 +6,13 @@ export default component$(() => {
             <h1>
                 About
             </h1>
+
+            <p>
+                Each little about section will be a full screen page
+            </p>
+            <p>
+                Effects: parallax and some css animating when coming onto the page (staggered)
+            </p>
         </div>
     );
 });
