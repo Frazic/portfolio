8805dce1f8a475c457bb6ee52f86997b71e90d28
Frazic <bruylant.joshua@gmail.com>
2022-11-10 12:26:45 +0100

    Talents fade in and out properly

--- src/routes/skills/index.tsx
+++ src/routes/skills/index.tsx
@@ -18,31 +18,83 @@ export default component$(() => {
             </div>
             <div className="tree-grid">
                 <div className="tree tree-1">
-                    <div className={"talent-grid"} onClick$={() => { isTree1Active.value = !isTree1Active.value; console.log(isTree1Active.value) }}>
+                    <div className={"talent-grid"} onClick$={() => { isTree1Active.value = !isTree1Active.value }}>
                         <div className="talent-header">Tree 1</div>
-                        <div className={isTree1Active.value ? "talent-column active" : "talent-column"}>
-                            <div style={{ "--order": "1" }} className="talent-item">Talent 1</div>
-                            <div style={{ "--order": "2" }} className="talent-item">Talent 1</div>
-                        </div>
-                        <div className={isTree1Active.value ? "talent-column active" : "talent-column"}>
-                            {[1, 2, 3, 4, 5].map((i) => (<div style={{ "--order": i }} className="talent-item">Talent {i}</div>))}
-                        </div>
+                        <TalentColumn isActive={isTree1Active.value}
+                            items={[
+                                { name: "Talent 1" },
+                                { name: "Talent 2" },
+                                { name: "Talent 3" }
+                            ]}
+                        />
+                        <TalentColumn isActive={isTree1Active.value}
+                            items={[
+                                { name: "Talent 1" },
+                                { name: "Talent 2" },
+                                { name: "Talent 3" }
+                            ]}
+                        />
+                        <TalentColumn isActive={isTree1Active.value}
+                            items={[
+                                { name: "Talent 1" },
+                                { name: "Talent 2" },
+                                { name: "Talent 3" }
+                            ]}
+                        />
                     </div>
                 </div>
-
                 <div className="tree tree-2">
-                    <div className={"talent-grid"} onClick$={() => { isTree2Active.value = !isTree2Active.value; console.log("2 " + isTree2Active.value) }}>
+                    <div className="talent-grid" onClick$={() => { isTree2Active.value = !isTree2Active.value }}>
                         <div className="talent-header">Tree 2</div>
                         <TalentColumn isActive={isTree2Active.value}
                             items={[
-                                { name: "Talent 1", order: 1 },
-                                { name: "Talent 2", order: 2 },
-                                { name: "Talent 3", order: 3 }
-                            ]} />
+                                { name: "Talent 1" },
+                                { name: "Talent 2" },
+                                { name: "Talent 3" }
+                            ]}
+                        />
+                        <TalentColumn isActive={isTree2Active.value}
+                            items={[
+                                { name: "Talent 1" },
+                                { name: "Talent 2" },
+                                { name: "Talent 3" }
+                            ]}
+                        />
+                        <TalentColumn isActive={isTree2Active.value}
+                            items={[
+                                { name: "Talent 1" },
+                                { name: "Talent 2" },
+                                { name: "Talent 3" }
+                            ]}
+                        />
                     </div>
                 </div>
-
-                <div className="tree tree-3">Tree 3</div>
+                <div className="tree tree-3">
+                    <div className="talent-grid" onClick$={() => { isTree3Active.value = !isTree3Active.value }}>
+                        <div className="talent-header">Tree 3</div>
+                        <TalentColumn isActive={isTree3Active.value}
+                            items={[
+                                { name: "Talent 1" },
+                                { name: "Talent 2" },
+                                { name: "Talent 3" }
+                            ]}
+                        />
+                        <TalentColumn isActive={isTree3Active.value}
+                            items={[
+                                { name: "Talent 1" },
+                                { name: "Talent 2" },
+                                { name: "Talent 3" }
+                            ]}
+                        />
+                        <TalentColumn isActive={isTree3Active.value}
+                            items={[
+                                { name: "Talent 1" },
+                                { name: "Talent 2" },
+                                { name: "Talent 3" }
+                            ]}
+                        />
+                    </div>
+                </div>
             </div>
         </>
     );
--- src/routes/skills/talentColumn.tsx
+++ src/routes/skills/talentColumn.tsx
@@ -1,24 +1,33 @@
-import { component$, useSignal, useStylesScoped$ } from "@builder.io/qwik";
+import { component$, useStylesScoped$ } from "@builder.io/qwik";
 import styles from "./skills.css";
 
 export interface TalentColumnProps {
     isActive: boolean,
     items: {
-        name: string,
-        order: number
+        name: string
     }[],
 }
 
 export const TalentColumn = component$((props: TalentColumnProps) => {
     useStylesScoped$(styles);
 
+    const createItems = () => {
+        const items: any[] = []
+        for (let index = 0; index < props.items.length; index++) {
+            const item = props.items[index];
+
+            // This flips the order on inactive so they fade out from bottom to top
+            const order = props.isActive ? index + 1 : props.items.length - index;
+
+            items.push(<TalentItem name={item.name} order={order} isActive={props.isActive} />);
+        }
+        return items;
+    }
+
     return (
-        // <div className={props.isActive ? "talent-column active" : "talent-column"}>
-        <div className={"talent-column"}>
+        <div className={props.isActive ? "talent-column active" : "talent-column"}>
             {
-                props.items.map((item) =>
-                    <TalentItem name={item.name} order={item.order} isActive={props.isActive} />
-                )
+                createItems()
             }
         </div>
     )
